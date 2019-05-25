c9cb903ab605eac5f18cba9e8792d7b42ba8b4d9
Khaelo <luca.bruno@ynov.com>
2019-05-25 02:16:41 +0200

    Administration ♥

---
+++ admin.lucabruno.fr/index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<html>
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
+
+    <title>Administration - LucaBruno.fr</title>
+
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+
+</head>
+<body>
+<h1>Hello World</h1>
+
+</body>
+</html>
